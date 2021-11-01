ec198e4e4097d79a0de4c823666eb98629d98304
Sandesh <sandesh.s@codilar.com>
2021-11-01 16:36:54 +0530

    first commit

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./Homepage.style.scss";
+const HomepageComponent = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomepageComponent;
